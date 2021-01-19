04a02d0088cb8324009fd9d5b8327ceade7be4bb
Fabio Z <zahnerfabio@gmail.com>
2021-01-19 11:21:23 +0100

    first placeholder for logo

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 var canvasW = 640;
 var canvasH = 480;
 
-function initCanvas()
-{
+function initCanvas(){
     canvas = document.getElementById("fsCanvas");
     canvas.width = document.body.clientWidth;
-    canvas.height = document.body.clientHeight; 
+    canvas.height = window.innerHeight; 
     canvasW = canvas.width;
     canvasH = canvas.height;
+    drawLogo();
+}
+
+function drawLogo(){
+    canvas = document.getElementById("fsCanvas");
+    ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95,50,40,0,2*Math.PI);
+    ctx.stroke();
+}
+
+function nextFrame(){
+    
 }
 
 initCanvas();
